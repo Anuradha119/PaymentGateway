ad4ff13a69cda3dda785c87d0bdabb7a31e304d1
Anuradha <marnianuradha@gmail.com>
2020-01-24 18:13:53 +0530

    fhghghj

--- PaymentGateway/ViewController.swift
+++ PaymentGateway/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Razorpay
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RazorpayPaymentCompletionProtocol {
+        func onPaymentError(_ code: Int32, description str: String) {
+            print("Payment unsuccessful")
+        }
+        
+        func onPaymentSuccess(_ payment_id: String) {
+            print("Payment successful")
+        }
 
+    
+    var razorpay:Razorpay!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        razorpay = Razorpay.initWithKey("rzp_live_MWuBuwjcEgv1L4"    , andDelegate: self)
+
         // Do any additional setup after loading the view.
     }
-
-
+        override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    func showPaymentForm(){
+        
+        let options: [String:Any] = [
+                        "amount": "100", //This is in currency subunits. 100 = 100 paise= INR 1.
+                        "currency": "INR",//We support more that 92 international currencies.
+                        "description": "purchase description",
+                        "order_id": "order_E7UCVznhOSxgo4",
+                        "image": "https://url-to-image.png",
+                        "name": "business or product name",
+                        "prefill": [
+                            "contact": "9797979797",
+                            "email": "foo@bar.com"
+                        ],
+                        "theme": [
+                            "color": "#F37254"
+                        ]
+                    ]
+            razorpay.open(options)
+        
+    }
+    
+    @IBAction func onPaymentBtnTap(_ sender: UIButton) {
+        showPaymentForm()
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-PaymentGatewayTests/Pods-PaymentGatewayTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PaymentGatewayTests : NSObject
+@end
+@implementation PodsDummy_Pods_PaymentGatewayTests
+@end
